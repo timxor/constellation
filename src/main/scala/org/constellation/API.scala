a45c37fc952cc4abaf008bee25df59bf0be7935b
Marcin Wadoń <mwadon@evojam.com>
2019-10-03 09:18:36 +0200

    Max tx per round: 30

--- src/main/scala/org/constellation/API.scala
+++ src/main/scala/org/constellation/API.scala
@@ -91,7 +91,7 @@ case class ProcessingConfig(
   snapshotTriggeringTimeSeconds: Int = 2,
   formUndersizedCheckpointAfterSeconds: Int = 30,
   numFacilitatorPeers: Int = 2,
-  maxTransactionsPerRound: Int = 3,
+  maxTransactionsPerRound: Int = 30,
   emptyTransactionsRounds: Int = 5,
   amountTransactionsRounds: Int = 5,
   metricCheckInterval: Int = 10,
